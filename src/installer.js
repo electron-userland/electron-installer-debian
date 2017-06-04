0e63a7941f5f3c029e995e832ccd0da9b710cfd9
Mark Lee <electronjs@lazymalevolence.com>
2017-06-03 17:19:28 -0700

    Fix typo

--- src/installer.js
+++ src/installer.js
@@ -128,7 +128,7 @@ var getDefaults = function (data, callback) {
       genericName: pkg.genericName || pkg.productName || pkg.name,
       description: pkg.description,
       productDescription: pkg.productDescription || pkg.description,
-      // Use '~' on pre-releases for proper debain version ordering.
+      // Use '~' on pre-releases for proper Debian version ordering.
       // See https://www.debian.org/doc/debian-policy/ch-controlfields.html#s-f-Version
       version: (pkg.version || '0.0.0').replace(/(\d)[_.+-]?((RC|rc|pre|dev|beta|alpha)[_.+-]?\d*)$/, '$1~$2'),
       revision: pkg.revision || '1',
