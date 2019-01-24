7d8a2b36efe9813c28f459013a8154304031d93a
Mark Lee <electronjs@lazymalevolence.com>
2019-01-23 20:56:49 -0800

    Remove tests now covered in -common

--- test/cli.js
+++ test/cli.js
@@ -1,15 +1,9 @@
 'use strict'
 
-const fs = require('fs-extra')
-const path = require('path')
-
 const access = require('./helpers/access')
-const dependencies = require('./helpers/dependencies')
-const describeInstaller = require('./helpers/describe_installer')
+const { cleanupOutputDir, tempOutputDir } = require('./helpers/describe_installer')
+const path = require('path')
 const spawn = require('../src/spawn')
-
-const cleanupOutputDir = describeInstaller.cleanupOutputDir
-const tempOutputDir = describeInstaller.tempOutputDir
 
 function runCLI (options) {
   const args = [
@@ -41,22 +35,6 @@ describe('cli', function () {
     runCLI({ src: 'test/fixtures/app-without-asar/', dest: outputDir, arch: 'amd64' })
 
     it('generates a `.deb` package', () => access(path.join(outputDir, 'bartest_0.0.1_amd64.deb')))
-
-    cleanupOutputDir(outputDir)
-  })
-
-  describe('with duplicate dependencies', function (test) {
-    const outputDir = tempOutputDir()
-    const config = 'test/fixtures/config.json'
-
-    runCLI({ src: 'test/fixtures/app-with-asar/', dest: outputDir, arch: 'i386', config: config })
-
-    it('removes duplicate dependencies', () =>
-      access(path.join(outputDir, 'footest_0.0.1_i386.deb'))
-        // object with both user and default dependencies based on src/installer.js
-        .then(() => fs.readJson(config))
-        .then(configObj => dependencies.assertDependenciesEqual(outputDir, 'footest_0.0.1_i386.deb', configObj))
-    )
 
     cleanupOutputDir(outputDir)
   })
